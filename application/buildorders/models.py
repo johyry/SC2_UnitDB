703db9b406f09cea13f3c2105fd0e9e3f73b6152
johyry <johannes.hyry@helsinki.fi>
2018-04-13 15:04:30 +0300

    fixing db row size

--- application/buildorders/models.py
+++ application/buildorders/models.py
@@ -5,11 +5,11 @@ from application.models import Base
 class Buildorder(Base):
 
     name = db.Column(db.String(144), nullable=False)
-    description = db.Column(db.String(1000))
-    intro = db.Column(db.String(1000))
+    description = db.Column(db.String(10000))
+    intro = db.Column(db.String(10000))
     vod = db.Column(db.String(144))
-    details = db.Column(db.String(1000))
-    analysis = db.Column(db.String(5000))
+    details = db.Column(db.String(10000))
+    analysis = db.Column(db.String(20000))
 
     def __init__(self, name):
         self.name = name
