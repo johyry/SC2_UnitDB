a5e9ce68e3a1cef46f30ac4f272f3008fc55f6f8
johyry <johannes.hyry@helsinki.fi>
2018-04-13 15:09:23 +0300

    adding more validators to createbuildorder for max size

--- application/buildorders/forms.py
+++ application/buildorders/forms.py
@@ -4,12 +4,12 @@ from application.units.models import Unit
  
 
 class BuildorderForm(FlaskForm):
-    name = TextField("Buildorder name", [validators.Length(min=2)])
-    description = StringField("Description")
-    intro = StringField("Introduction")
-    vod = StringField("VOD", [validators.Length(max=100)])
-    details = StringField("Buildorder")
-    analysis = StringField("Analysis")
+    name = TextField("Buildorder name", [validators.Length(max=100, message="Input must be less than 100 characters")])
+    description = StringField("Description", [validators.Length(max=10000, message='Input must be less than 10 000 characters')])
+    intro = StringField("Introduction", [validators.Length(max=10000, message='Input must be less than 10 000 characters')])
+    vod = StringField("VOD", [validators.Length(max=100, message='Input must be less than 100 characters')])
+    details = StringField("Buildorder", [validators.Length(max=10000, message='Input must be less than 10 000 characters')])
+    analysis = StringField("Analysis", [validators.Length(max=10000, message='Input must be less than 10 000 characters')])
 
     class Meta:
         csrf = False
